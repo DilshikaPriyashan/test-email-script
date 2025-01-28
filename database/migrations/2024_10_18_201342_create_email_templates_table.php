c6dd5b984d302d31b609b388c0934ce8e3aaf641
Dilshika <dilshika@thesanmark.com>
2025-01-28 11:49:18 +0530

    add again defaulte value

--- database/migrations/2024_10_18_201342_create_email_templates_table.php
+++ database/migrations/2024_10_18_201342_create_email_templates_table.php
@@ -18,8 +18,8 @@ return new class extends Migration
             $table->string('name');
             $table->string('subject');
             $table->string('api_ref');
-            $table->json('attributes');
-            $table->longText('content');
+            $table->json('attributes')->default('[]');
+            $table->longText('content')->default('');
             $table->enum('auth_mechanism', array_column(EmailAuthMethods::cases(), 'value'))->default(EmailAuthMethods::NONE->value);
             $table->foreignIdFor(Team::class)->constrained()->cascadeOnDelete();
             $table->timestamps();
